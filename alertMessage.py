edd19d3ea47339a26b774581d258f1ed0367e800
ilukgolf <jgbbmrssk1909@outlook.com>
2022-11-27 23:25:12 +0700

    Update alertMessage.py

--- alertMessage.py
+++ alertMessage.py
@@ -1,5 +1,17 @@
-import tkinter.messagebox as msg
+from tkinter import messagebox as msg
 
+def databaseError(errno, _db):
+    msg.showerror('Database Error', 'MySQL Error Code: {:<35}\n\n{}'.format(errno, mySQL_error_code(errno, _db)))
+    
+def mySQL_error_code(errno, _db):
+    if errno == 2005:
+        return str("Unknown MySQL server host '{0}'".format(_db['mysqlServer']))
+    elif errno == 1045:
+        return str("Access denied for user '{0}'@'{1}'".format(_db['Username'], _db['mysqlServer']))
+    elif errno == 1049:
+        return str("Unknown database '{0}'".format(_db['databaseTable']))
+    elif errno == 2003:
+        return str("Can't connect to MySQL server on '{0}'".format(_db['mysqlServer']))
 
 if __name__ == "__main__":
     import main
