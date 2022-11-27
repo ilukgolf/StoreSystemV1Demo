b0a35e5f71916d6ceb909b6e0a68b11dfa702f71
ilukgolf <jgbbmrssk1909@outlook.com>
2022-11-28 01:59:08 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.font as tkFont
 import config, alertMessage as alert
 
-def mainFrame(root):
+def mainFrame(root, userInfo):
     frame = tk.Frame(root, background=config.colorBackground)
     
     def generateUI():
