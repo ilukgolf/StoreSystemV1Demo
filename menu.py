bfda4ea73df598c583e37049e8177c48012483f5
ilukgolf <jgbbmrssk1909@outlook.com>
2022-11-27 21:41:44 +0700

    add menu ui

---
+++ menu.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import config, alertMessage as alert
+
+def mainFrame(root):
+    frame = tk.Frame(root, background=config.colorBackground)
+    
+    def generateUI():
+        pass #! CODE HERE
+    
+    
+    return frame
+
+if __name__ == "__main__":
+    import main
+    main.mainWindow()
