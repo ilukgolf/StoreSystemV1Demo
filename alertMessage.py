d01379bc8264564eeea688d9b2cda6dd770c3220
ilukgolf <jgbbmrssk1909@outlook.com>
2022-11-28 01:59:03 +0700

    Update alertMessage.py

--- alertMessage.py
+++ alertMessage.py
@@ -1,7 +1,7 @@
 from tkinter import messagebox as msg
 
 def databaseError(errno, _db):
-    msg.showerror('Database Error', 'MySQL Error Code: {:<35}\n\n{}'.format(errno, mySQL_error_code(errno, _db)))
+    msg.showerror('ฐานข้อมูลผิดพลาด', 'MySQL Error Code: {:<35}\n\n{}'.format(errno, mySQL_error_code(errno, _db)))
     
 def mySQL_error_code(errno, _db):
     if errno == 2005:
@@ -12,6 +12,25 @@ def mySQL_error_code(errno, _db):
         return str("Unknown database '{0}'".format(_db['databaseTable']))
     elif errno == 2003:
         return str("Can't connect to MySQL server on '{0}'".format(_db['mysqlServer']))
+
+def login_success(user):
+    msg.showinfo("เข้าสู่ระบบสำเร็จ", "ยินดีต้อนรับคุณ %s เข้าสู่ระบบ" % (user))
+    
+def login_error(code, user = None):
+    if code == 0:
+        msg.showerror("เกิดข้อผิดพลาด", "พบข้อมูลผู้ใช้จำนวน %s รายการซ้ำภายในระบบ\nกรุณาติดต่อผู้ดูแลระบบ" % (user))
+    elif code == 1:
+        msg.showwarning("เข้าสู่ระบบไม่สำเร็จ", "{:<35}".format("กรุณากรอกชื่อผู้ใช้ และรหัสผ่านให้ครบถ้วน"))
+    elif code == 2:
+        msg.showwarning("เข้าสู่ระบบไม่สำเร็จ", "{:<35}".format("กรุณากรอกชื่อผู้ใช้"))
+    elif code == 3:
+        msg.showwarning("เข้าสู่ระบบไม่สำเร็จ", "{:<35}".format("กรุณากรอกรหัสผ่าน"))
+    elif code == 4:
+        msg.showwarning("เข้าสู่ระบบไม่สำเร็จ", "{:<35}".format("ชื่อผู้ใช้งาน หรือรหัสผ่านไม่ถูกต้อง"))
+    elif code == 5:
+        msg.showwarning("เข้าสู่ระบบไม่สำเร็จ", "{:<35}".format("บัญชีถูกระงับการใช้งาน กรุณาติดต่อผู้ดูแลระบบ"))
+    else:
+        msg.showerror("เกิดข้อผิดพลาด", "{:<35}".format("เกิดข้อผิดพลาดที่ไม่รู้จัก กรุณาติดต่อผู้ดูแลระบบ"))
 
 if __name__ == "__main__":
     import main
